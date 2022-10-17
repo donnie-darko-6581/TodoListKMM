3c15a64feba418bf2c2292f62c7e85ed72f2fe07
donnie-darko-6581 <patil.lalit.v3@gmail.com>
2022-10-17 19:35:55 +0530

    feat: checking serialisable annotation

--- shared/src/commonMain/kotlin/com/example/api/models/Entity.kt
+++ shared/src/commonMain/kotlin/com/example/api/models/Entity.kt
@@ -1,10 +1,14 @@
 package com.example.api.models
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class EntityResponse(
     val count: Int,
     val entries: List<Entity>
 )
 
+@Serializable
 data class Entity(
     val API: String,
     val Auth: String,
