8fb8b9cef04dcd48a7efb6224e33c3016964755f
donnie-darko-6581 <patil.lalit.v3@gmail.com>
2022-10-19 12:26:02 +0530

    feat: fixed generics.

--- shared/src/commonMain/kotlin/com/example/usecases/GetEntriesUseCase.kt
+++ shared/src/commonMain/kotlin/com/example/usecases/GetEntriesUseCase.kt
@@ -1,6 +1,7 @@
 package com.example.usecases
 
 import com.example.api.impl.EntriesListImpl
+import com.example.api.models.EntityResponse
 import com.example.helper.Result
 import com.example.helper.resultHandler
 import kotlinx.coroutines.CoroutineDispatcher
@@ -10,7 +11,7 @@ class GetEntriesUseCase(
     private val repository: EntriesListImpl
 ) {
 
-    suspend fun getEntries(): Result<Unit> {
+    suspend fun getEntries(): Result<EntityResponse> {
         return resultHandler(dispatcher) { repository.entries() }
     }
 
