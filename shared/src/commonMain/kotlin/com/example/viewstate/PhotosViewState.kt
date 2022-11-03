a0ea00c15b92714037d7b02d1e282b08c3d1d9cc
donnie-darko-6581 <patil.lalit.v3@gmail.com>
2022-11-03 13:28:27 +0530

    feat: sealed class removal for ios.

--- shared/src/commonMain/kotlin/com/example/viewstate/PhotosViewState.kt
+++ shared/src/commonMain/kotlin/com/example/viewstate/PhotosViewState.kt
@@ -4,10 +4,11 @@ import com.example.api.BaseApiViewState
 import com.example.api.models.PhotosResponseItem
 import com.example.helper.Result
 
-data class PhotosViewState(
+data class PhotosViewState constructor(
     override val response: Result<List<PhotosResponseItem>>?,
     override val isLoading: Boolean,
-    override val error: Exception?
+    override val error: Exception?,
+    val list: List<PhotosResponseItem>? = null // todo: ios kotlin sealed class casting wont work so temp soln
 ): BaseApiViewState<List<PhotosResponseItem>> {
     companion object {
         fun loading() = PhotosViewState(
@@ -19,7 +20,8 @@ data class PhotosViewState(
         fun success(response: Result<List<PhotosResponseItem>>) = PhotosViewState(
             response = response,
             isLoading = false,
-            error = null
+            error = null,
+            list = (response as Result.Success<List<PhotosResponseItem>>).data
         )
     }
 
